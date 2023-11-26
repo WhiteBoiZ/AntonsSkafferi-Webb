af032723bb287c325896b66e91a6d1160738a57b
Ruben Larsson <rubenlarsson97@gmail.com>
2023-11-27 00:29:46 +0100

    Lagt till PrimeFlex som dependency. Försökt skapa serverrepresentation av vald lunch men kan inte visa det på sidan.

--- src/main/java/Lunch.java
+++ src/main/java/Lunch.java
@@ -1,12 +1,26 @@
+import java.util.HashMap;
+
 public class Lunch {
     private String title;
     private String description;
+    private Boolean[] daySelected = new Boolean[5];
 
     public Lunch(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
+    public void selectDay(int day){
+        daySelected[day] = true;
+    }
+
+    public void unselectDay(int day){
+        daySelected[day] = false;
+    }
+
+    public void putSelection(int day, boolean choice){
+        daySelected[day] = choice;
+    }
     public String getTitle() {
         return title;
     }
@@ -21,5 +35,13 @@ public class Lunch {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Boolean[] getDaySelected() {
+        return daySelected;
+    }
+
+    public void setDaySelected(Boolean[] daySelected) {
+        this.daySelected = daySelected;
     }
 }
--- src/main/java/LunchBean.java
+++ src/main/java/LunchBean.java
@@ -1,11 +1,11 @@
+import jakarta.el.MethodExpression;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.model.ListDataModel;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 @ApplicationScoped
 @Named
@@ -14,6 +14,10 @@ public class LunchBean {
     private String lunchDescription;
     private ArrayList<Lunch> lunches = new ArrayList<>();
     private ListDataModel<Lunch> listModel = new ListDataModel<Lunch>(lunches);
+    private Lunch[] lunchMenu = new Lunch[5];
+
+    private String[] selected;
+
 
 
     // Getters and setters for title and description
@@ -42,11 +46,88 @@ public class LunchBean {
         return listModel;
     }
 
-    public String now(){
-        return new Date().toString();
+    public void setLunchMenu() {
+        int lunchIndex = listModel.getRowIndex();
+        lunchMenu[lunchIndex] = lunches.get(lunchIndex);
     }
 
+    public List<Boolean> getSelection(){
+        List<Boolean> selection = Arrays.asList(new Boolean[5]);
+        for(int i = 0; i < lunchMenu.length; i++) {
+            if(lunchMenu[i] == lunches.get(listModel.getRowIndex())) {
+                selection.set(i, true);
+            }
+        }
+        return selection;
+    }
 
+    public String[] getSelected() {
+        ArrayList<String> sel = new ArrayList<>();
+        Lunch currentLunch = lunches.get(listModel.getRowIndex());
+        for(int i = 0; i < currentLunch.getDaySelected().length; i++){
+            if(currentLunch.getDaySelected()[i]){
+                sel.add(dayIntToString(i));
+            }
+        }
+        return sel.toArray(new String[sel.size()]);
+    }
 
+    public void setSelected(String[] selected) {
+        this.selected = selected;
+        for(String sel:selected){
+            System.out.println(sel);
+        }
+    }
 
+    public void selectedOptionsChanged() {
+        Lunch currentLunch = lunches.get(listModel.getRowIndex());
+        Arrays.fill(currentLunch.getDaySelected(), false);
+        if (selected != null){
+            for(String sel: selected){
+                currentLunch.selectDay(dayStringToInt(sel));
+            }
+        }
+    }
+
+    private static int dayStringToInt(String day){
+        switch (day) {
+            case "man":
+                return 0;
+            case "tis":
+                return 1;
+            case "ons":
+                return 2;
+            case "tor":
+                return 3;
+            case "fre":
+                return 4;
+            case "lor":
+                return 5;
+            case "son":
+                return 6;
+            default:
+                return 100;
+        }
+    }
+
+    private static String dayIntToString(int day){
+        switch (day) {
+            case 0:
+                return "man";
+            case 1:
+                return "tis";
+            case 2:
+                return "ons";
+            case 3:
+                return "tor";
+            case 4:
+                return "fre";
+            case 5:
+                return "lor";
+            case 6:
+                return "son";
+            default:
+                return "100";
+        }
+    }
 }
