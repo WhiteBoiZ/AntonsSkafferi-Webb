5e68d8f8f8b01dcb6707b7347d4cc73de1515494
greger12 <emil9393@hotmail.com>
2023-11-29 21:40:48 +0100

    Added preparation for ala cart page.

---
+++ src/main/java/se/miun/whiteboiz/AlacartBean.java
@@ -0,0 +1,11 @@
+package se.miun.whiteboiz;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+@ApplicationScoped
+@Named
+public class AlacartBean {
+
+
+}
---
+++ src/main/java/se/miun/whiteboiz/containers/Alacart.java
@@ -0,0 +1,43 @@
+package se.miun.whiteboiz.containers;
+
+public class Alacart {
+    public enum AlaCarteCategory{
+        STARTER, MAIN, DESSERT
+    }
+    private String title;
+    private String description;
+    private AlaCarteCategory alaCarteCategory;
+
+    public Alacart(AlaCarteCategory category, String title, String description){
+        this.alaCarteCategory = category;
+        this.title = title;
+        this.description = description;
+
+
+    }
+    //Getters & Setters
+    public AlaCarteCategory getAlaCarteCategory(){
+        return alaCarteCategory;
+    }
+    public void setAlaCarteCategory(AlaCarteCategory category){
+        this.alaCarteCategory = category;
+    }
+    public String getTitel() {
+        return title;
+    }
+
+    public void setTitel(String titel) {
+        this.title = title;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
