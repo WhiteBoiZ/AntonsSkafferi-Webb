a13fe751cbf3b12aa32d2171a95fda6f120ea9c4
Ruben Larsson <rubenlarsson97@gmail.com>
2023-12-04 22:47:17 +0100

    mergat

--- src/main/java/se/miun/whiteboiz/entities/LunchEntity.java
+++ src/main/java/se/miun/whiteboiz/entities/LunchEntity.java
@@ -3,7 +3,7 @@ package se.miun.whiteboiz.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "lunch", schema = "antonsskafferi", catalog = "")
+@Table(name = "lunch", schema = "antonsskafferi")
 public class LunchEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -40,25 +40,4 @@ public class LunchEntity {
         this.beskrivning = beskrivning;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        LunchEntity that = (LunchEntity) o;
-
-        if (id != that.id) return false;
-        if (titel != null ? !titel.equals(that.titel) : that.titel != null) return false;
-        if (beskrivning != null ? !beskrivning.equals(that.beskrivning) : that.beskrivning != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (titel != null ? titel.hashCode() : 0);
-        result = 31 * result + (beskrivning != null ? beskrivning.hashCode() : 0);
-        return result;
-    }
 }
