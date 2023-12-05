3e1ea9a134c19c72a7dde3ae507520fff3599d70
eqzpert <rubenlarsson97@gmail.com>
2023-12-05 16:13:34 +0100

    stuff

--- src/main/webapp/resources/database/db.sql
+++ src/main/webapp/resources/database/db.sql
@@ -37,7 +37,11 @@ create table alacarte(
     pris int,
     primary key (id)
 );
-
+create table bord(
+                     id int not null,
+                     status varchar(255) not null,
+                     primary key (id)
+);
 create table bestallning(
     id int auto_increment not null,
     datum date not null,
@@ -57,11 +61,7 @@ create table ratt_instans(
 
 );
 
-create table bord(
-    id int not null,
-    status varchar(255) not null,
-    primary key (id)
-);
+
 
 insert into dag (id, namn) values (1, 'Måndag');
 insert into dag (id, namn) values (2, 'Tisdag');
